c864f017654e291fac0648ff492301213f3d16e7
vaibhav <vaibhavsutar264>
2023-03-13 13:43:58 +0530

    changhes for unhandled exception

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const app = require("./server");
 // console.log(youtube);
 
 
+
+
+
 const server = app.listen(process.env.PORT || 8585,()=>{
 
     console.log(`Server is running on http://localhost:${process.env.PORT}`) //this is port on which we will work for backend
 })
 
+
+process.on("unhandledRejection",(err)=>{
+    console.log(`Error:${err.message}`);
+    console.log(`shutting down the pc due to unhandled promise rejection`);
+
+    server.close(()=>{
+        process.exit(1);
+    });
+});
 
